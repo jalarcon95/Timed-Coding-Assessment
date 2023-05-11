2e97f626881f002c8288dad3ee4166b779089f59
jalarcon95 <130102758+jalarcon95@users.noreply.github.com>
2023-05-11 18:53:44 -0400

    started defining variables in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var score = 0;
+var timer = 60;
+var index = 0;
+var saveHighScore = [];
+var containerBtn = document.querySelector(".textAction");
+var container = document.querySelector(".textBox");
+var textQuestion = document.querySelector(".textCenter");
+var answer = document.querySelector(".answer");
+var linkScore = document.querySelector(".a-score");
+var remainTime = document.querySelector(".timer");
+var firstColor = "#4112cf";
+var secondColor = "#1778ad";
+var textCnt = "left";
+
