43da25eaf2a8ba9e50328cef599cc8bc935edf77
jalarcon95 <130102758+jalarcon95@users.noreply.github.com>
2023-05-15 19:05:17 -0400

    created function for button to start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,10 @@ var answer = document.querySelector(".answer");
 var linkScore = document.querySelector(".a-score");
 var remainTime = document.querySelector(".timer");
 var firstColor = "#4112cf";
-var secondColor = "#1778ad";
+var secondColor = "#5fc6fe";
 var textCnt = "left";
+
+initialContent();
 
 var questions = [
 
@@ -65,4 +67,32 @@ var questions = [
 
     },
 
-]
+];
+
+function createButton (id, txt){
+    var button = document.createElement("button");
+    button.setAttribute("id", id);
+    button.textContent = txt;
+    button.style.background = firstColor;
+    button.style.color = secondColor;
+    button.style.borderRadius = "20px"
+    containerBtn.appendChild(button);
+
+}
+
+function initialContent()
+{
+    createButton("start", "Start Quiz");
+
+    highOption: 0;
+}
+
+var checkTimer = function(){
+    var eval = false;
+    if(timer <= 0){
+        timer = 0;
+        eval = true
+    }
+    return eval;
+};
+
